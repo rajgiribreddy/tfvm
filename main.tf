b7324d6ed983f6bbf28b04343c8a30d827a22317
rajgiribreddy <78073796+rajgiribreddy@users.noreply.github.com>
2021-03-06 11:29:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "azurerm_network_security_group" "main" {
 
   }
 }
-resource "azurerm_network_security_group_association" "main" {
+resource "azurerm_network_interface_security_group_association" "main" {
   network_interface_id      = azurerm_network_interface.main.id
   network_security_group_id = azurerm_network_security_group.main.id
 }
