6dced75d2d97c4466bd5ca7ae2a86ae4e36ed469
Rajgiri B Reddy <rajgiribreddy@gmail.com>
2021-03-06 10:20:08 +0530

    updated

---
+++ main.tf
@@ -0,0 +1,80 @@
+resource "azurerm_resource_group" "main" {
+  name     = "VM-RG"
+  location = "eastus"
+}
+resource "azurerm_virtual_network" "main" {
+  name                = "VM-VNET"
+  address_space       = ["14.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "VM-SUBNET"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefix       = "14.0.0.0/24"
+}
+resource "azurerm_public_ip" "main" {
+  name                = "VM-PIP"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  allocation_method   = "Dynamic"
+  tags = {
+    environment = "Prouction"
+  }
+}
+resource "azuererm_network_interface" "main" {
+  name                = "VM-NIC"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  ip_configuration {
+    name                          = "VM-IPNAME"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
+  }
+}
+resource "azurerm_network_security_group" "main" {
+  name     = "VM-NSG"
+  location = azurerm_resource_group.main.location
+  security_rule = {
+    access                     = "Allow"
+    direction                  = "Inbound"
+    name                       = "RDP"
+    priority                   = 100
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    source_address_prefix      = "*"
+    destination_port_range     = "3389"
+    destination_address_prefix = "*"
+
+  }
+}
+resource "azurerm_network_security_group_association" "main" {
+  network_interface_id      = azurerm_network_interface.main.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
+
+resource "azurerm_windows_virtual_machine" "main" {
+  name                = "VM1"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  size                = "Standard_DS1_V2"
+  admin_username      = "adminuser"
+  admin_password      = "Azure@123456"
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+}
---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  features {}
+ 
+  subscription_id = "a6b5ec67-c7fa-4e3e-8ff0-ccf20b91e159"
+  client_id       = "3106fc53-de24-41ac-91ab-6d18dc54787b"
+  client_secret   = "A_P34Bj.74W84.s47~u~u69j~7lU97I.Wf"
+  tenant_id       = "1abe5117-1b2e-4bc3-99e1-64cdca6451cb"
+}
