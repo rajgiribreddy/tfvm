56d4a3a8b93d2a3ecc878d2d1233627066fe1d51
rajgiribreddy <78073796+rajgiribreddy@users.noreply.github.com>
2021-03-06 11:16:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "azurerm_public_ip" "main" {
     environment = "Prouction"
   }
 }
-resource "azuererm_network_interface" "main" {
+resource "azurerm_network_interface" "main" {
   name                = "VM-NIC"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
